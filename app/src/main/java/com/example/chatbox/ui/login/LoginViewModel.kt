437f957dd8c9215ff8fc83db573eedb41e6f6476
aliciamj613 <alicia_meijun_613@163.com>
2025-11-30 16:36:06 +0800

    chore: init chatbox project with compose

--- app/src/main/java/com/example/chatbox/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/example/chatbox/ui/login/LoginViewModel.kt
@@ -1,25 +1,9 @@
 package com.example.chatbox.ui.login
 
-import androidx.lifecycle.ViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.update
+
 
 data class LoginUiState(
     val userId: String = "",
     val isLoggingIn: Boolean = false
 )
 
-class LoginViewModel : ViewModel() {
-
-    private val _uiState = MutableStateFlow(LoginUiState())
-    val uiState: StateFlow<LoginUiState> = _uiState
-
-    fun onUserIdChange(text: String) {
-        _uiState.update { it.copy(userId = text) }
-    }
-
-    fun setLogging(logging: Boolean) {
-        _uiState.update { it.copy(isLoggingIn = logging) }
-    }
-}
